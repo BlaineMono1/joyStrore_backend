912fea77d26e624e6bd96cd67acf3a70d680c37b
zxcuser1 <126661647+zxcuser1@users.noreply.github.com>
2025-02-01 15:54:55 +0300

    Added CRUD operations in repository

--- DataBaseToAccess/Repositiory/Repository.cs
+++ DataBaseToAccess/Repositiory/Repository.cs
@@ -16,7 +16,51 @@ namespace DataBaseToAccess.Repositiory
         /// <returns></returns>
         public async Task<List<T>> GetAllList()
         {
-             return await _context.Set<T>().AsNoTracking().Where(p => !p.IsDelete).ToListAsync();
+             return await _context.Set<T>().AsNoTracking().Where(e => !e.IsDelete).ToListAsync();
+        }
+
+        /// <summary>
+        /// Получить объект по id 
+        /// </summary>
+        /// <returns></returns>
+        public async Task<T?> GetById(Guid id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        /// <summary>
+        /// Обновить объект 
+        /// </summary>
+        /// <returns></returns>
+        public async Task Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        /// <summary>
+        /// Удалить объект 
+        /// </summary>
+        /// <returns></returns>
+        public async Task Delete(Guid id)
+        {
+            var entity = await GetById(id);
+            if (entity != null)
+            {
+                entity.IsDelete = true;
+                await Update(entity);
+            }
+            
+        }
+
+        /// <summary>
+        /// Добавить объект 
+        /// </summary>
+        /// <returns></returns>
+        public async Task Add(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
         }
     }
 }
