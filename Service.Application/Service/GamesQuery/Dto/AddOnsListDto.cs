1c703f9dc6472edded784ff4891f2d10b2d56d67
zxcuser1 <126661647+zxcuser1@users.noreply.github.com>
2025-02-05 15:07:51 +0300

    Вывод игр на главной странице

---
+++ Service.Application/Service/GamesQuery/Dto/AddOnsListDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Service.Application.Service.GamesQuery.Dto
+{
+    public class AddOnsListDto
+    {
+        public Guid Id { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
