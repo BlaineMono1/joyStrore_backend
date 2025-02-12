a9dde0d66bd8803932d74a2407551e5ada1ccf1e
zxcuser1 <126661647+zxcuser1@users.noreply.github.com>
2025-02-12 13:42:13 +0300

    GET - запрос на получение личного кабинета

---
+++ DataBaseToAccess/Repositiory/RepositoryEntity/UserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Business.Data.Iterfaces;
+using Business.Data.Iterfaces.Store;
+using Business.Data.Models;
+
+namespace DataBaseToAccess.Repositiory.RepositoryEntity
+{
+    public class UserRepository<T> : Repository<User>, IUserRepository<T> where T : class, IBaseEntity
+    {
+        public UserRepository(BaseDbContext contex) : base(contex) { }
+
+        public async Task<User> GetUserByTgId(string tgId)
+        {
+            return (await GetAllList()).FirstOrDefault(u => u.TgUserId == tgId) ?? throw new Exception($"No User with {tgId} tg id");
+        }
+    }
+}
