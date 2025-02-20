765141169cdca8170712be06ecd0bdbe92c79fe0
zxcuser1 <126661647+zxcuser1@users.noreply.github.com>
2025-02-20 18:21:42 +0300

    fix migrations

---
+++ DataBaseToAccess/BaseDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using System.IO;
+
+namespace DataBaseToAccess
+{
+    public class BaseDbContextFactory : IDesignTimeDbContextFactory<BaseDbContext>
+    {
+        public BaseDbContext CreateDbContext(string[] args)
+        {
+            
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<BaseDbContext>();
+
+            
+            var connectionString = configuration.GetConnectionString("DataBaseConnection");
+            optionsBuilder.UseNpgsql(connectionString);
+
+            return new BaseDbContext(optionsBuilder.Options);
+        }
+    }
+}
