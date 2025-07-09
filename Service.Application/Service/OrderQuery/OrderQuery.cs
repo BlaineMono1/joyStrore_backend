ed9dc1babea78d780133c7aead0e8612b7ee8f9e
zxcuser1 <126661647+zxcuser1@users.noreply.github.com>
2025-07-09 17:33:20 +0300

    fix

--- Service.Application/Service/OrderQuery/OrderQuery.cs
+++ Service.Application/Service/OrderQuery/OrderQuery.cs
@@ -422,7 +422,7 @@ namespace Service.Application.Service.OrderQuery
                 var orderItem = new OrderProductItem
                 {
                     Price = await _calculatePrice.CalcPrice(product.PriceUa, product.PriceTr, product.Type),
-                    Discount = (region == "UAH" ? product.DiscountPercentUa : product.DiscountPercentTr),
+                    Discount = (region == "UAH" ? product.DiscountPercentUa ?? "" : product.DiscountPercentTr ?? ""),
                     OrderId = order.Guid,
                     ProductId = product.Guid
                 };
