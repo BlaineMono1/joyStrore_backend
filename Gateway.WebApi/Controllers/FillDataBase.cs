1b00647c557f1baa4bab8c20da92eb54de3c34d8
zxcuser1 <126661647+zxcuser1@users.noreply.github.com>
2025-07-27 23:05:49 +0300

    parse games

--- Gateway.WebApi/Controllers/FillDataBase.cs
+++ Gateway.WebApi/Controllers/FillDataBase.cs
@@ -17,87 +17,15 @@ namespace Gateway.WebApi.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult> FillBd()
+        public async Task<ActionResult> ParseGames(int startPage, int endPage)
         {
             try
             {
-                await _parse.StartParse();
+                await _parse.ParseGames(startPage, endPage);
                 return Ok();
             }
             catch (Exception ex)
             {                
-                return StatusCode(500, "An error occurred while parsing");
-            }
-        }
-
-        [HttpGet]
-        public async Task<ActionResult> RegUser()
-        {
-            try
-            {
-                await _parse.RegUser();
-                return Ok();
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, "An error occurred while reg user");
-            }
-        }
-
-        [HttpGet]
-        public async Task<ActionResult> CreateSection()
-        {
-            try
-            {
-                await _parse.CreateSections();
-                return Ok();
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, "An error occurred while create sections");
-            }
-        }
-
-
-        [HttpGet]
-        public async Task<ActionResult> CreateGamesMarkUp()
-        {
-            try
-            {
-                await _parse.CreateGameMarcup();
-                return Ok();
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, "An error occurred while parsing");
-            }
-        }
-
-        [HttpGet]
-        public async Task<ActionResult> CreateSub()
-        {
-            try
-            {
-                await _parse.CreatuSub();
-                return Ok();
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, ex.Message);
-            }
-        }
-
-
-        [HttpGet]
-        public async Task<ActionResult> CreateAddOns()
-        {
-            try
-            {
-                await _parse.Create_addOn();
-                return Ok();
-            }
-            catch (Exception ex)
-            {
                 return StatusCode(500, ex.Message);
             }
         }
