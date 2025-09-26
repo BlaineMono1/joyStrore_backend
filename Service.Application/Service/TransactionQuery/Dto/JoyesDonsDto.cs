c74ef2b11e8cd61ba54517b930a388617c02aabb
Yra <y.seryakov@bilaboratory.com>
2025-09-26 19:12:50 +0300

    оплата joy в бота

--- Service.Application/Service/TransactionQuery/Dto/JoyesDonsDto.cs
+++ Service.Application/Service/TransactionQuery/Dto/JoyesDonsDto.cs
@@ -1,9 +1,10 @@
-﻿
-namespace Service.Application.Service.TransactionQuery.Dto
+﻿namespace Service.Application.Service.TransactionQuery.Dto
 {
     public class JoyesDonsDto
     {
-        public List<decimal> Joy { get; set; } = new List<decimal> { 500, 1000, 2500, 5000, 10000};
-        public List<decimal> JoyPlus { get; set; } = new List<decimal> {100, 500, 1000, 2500, 5000, 10000 };
+        public List<decimal> Joy { get; set; } =
+            new List<decimal> { 15, 500, 1000, 2500, 5000, 10000 };
+        public List<decimal> JoyPlus { get; set; } =
+            new List<decimal> { 100, 500, 1000, 2500, 5000, 10000 };
     }
 }
