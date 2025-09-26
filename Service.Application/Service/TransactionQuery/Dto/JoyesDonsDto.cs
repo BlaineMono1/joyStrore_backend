8d0f244d1e4b8e4691e0d88dd65469182312b23e
Yra <y.seryakov@bilaboratory.com>
2025-09-26 20:27:29 +0300

    fix

--- Service.Application/Service/TransactionQuery/Dto/JoyesDonsDto.cs
+++ Service.Application/Service/TransactionQuery/Dto/JoyesDonsDto.cs
@@ -2,8 +2,7 @@
 {
     public class JoyesDonsDto
     {
-        public List<decimal> Joy { get; set; } =
-            new List<decimal> { 15, 500, 1000, 2500, 5000, 10000 };
+        public List<decimal> Joy { get; set; } = new List<decimal> { 500, 1000, 2500, 5000, 10000 };
         public List<decimal> JoyPlus { get; set; } =
             new List<decimal> { 100, 500, 1000, 2500, 5000, 10000 };
     }
